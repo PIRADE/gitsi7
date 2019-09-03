08e0d9ea18d1387d05850a3d9060fae8d2208ddf
PIRADE <epeenaert@orange.fr>
2019-09-03 15:26:44 +0200

    version 2

--- chapitre_include.php
+++ chapitre_include.php
@@ -22,8 +22,9 @@
 </nav> <!-- fermeture navbar -->
 
 <div id="contenu_1">
-	Ici mon bloc 1
+	Ici mon bloc principal du fichier php
 </div> <!-- fermeture contenu_1 -->
+
 
 <div id="contenu_2">
 	Ici mon bloc 2
